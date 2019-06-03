be1a47878a69db10d4ceafc80e4cfb7929dc5182
cma-1982 <coredmotor1982@gmail.com>
2019-06-03 17:30:27 +0900

    家のＰＣでビルド通った

--- default190601/i_src/r_pdl_user_definitions.h
+++ default190601/i_src/r_pdl_user_definitions.h
@@ -9,7 +9,7 @@
 * Version      : 
 * Description  : User-modifiable definitions
 ******************************************************************************
-* History : 02.06.2019 Version Description
+* History : 03.06.2019 Version Description
 *         :   
 ******************************************************************************/
 
